dceb205781deab3d27f724b7578dc80c11827543
Andrew V. Kharchenko <andrew.v.kharchenko@gmail.com>
2015-04-07 05:57:39 +0300

    Gitignore configured

--- include/config.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_CONFIG_HPP_
-#define INCLUDE_CONFIG_HPP_
-// the configured options and settings for Tutorial
-#define LFDS_VERSION_MAJOR 1
-#define LFDS_VERSION_MINOR 0
-#define LFDS_USE_CPP11 0
-
-#endif // INCLUDE_CONFIG_HPP_
